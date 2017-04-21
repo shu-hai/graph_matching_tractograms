61cc0d21fef8c029c87da8d4ed31e96104930dde
Emanuele Olivetti <emanuele.olivetti@gmail.com>
2017-04-21 10:11:00 +0200

    major refactoring + changed default threshold for discaring short streamlines

--- alignment_as_gm.py
+++ alignment_as_gm.py
@@ -4,6 +4,7 @@ See: Olivetti E, Sharmin N and Avesani P (2016) Alignment of
 Tractograms As Graph Matching. Front. Neurosci. 10:554. 
 doi:10.3389/fnins.2016.00554
 
+Author: Emanuele Olivetti
 """
 
 from __future__ import print_function
@@ -22,6 +23,22 @@ try:
     joblib_available = True
 except ImportError:
     joblib_available = False
+
+
+def load_tractogram(T_filename, threshold_short_streamlines=10.0):
+    """Load tractogram from TRK file and remove short streamlines with
+    length below threshold.
+    """
+    print("Loading %s" % T_filename)
+    T, hdr = trackvis.read(T_filename, as_generator=False)
+    T = np.array([s[0] for s in T], dtype=np.object)
+    print("%s: %s streamlines" % (T_filename, len(T)))
+
+    # Removing short artifactual streamlines
+    print("Removing (presumably artifactual) streamlines shorter than %s" % threshold_short_streamlines)
+    T = np.array([s for s in T if length(s) >= threshold_short_streamlines], dtype=np.object)
+    print("%s: %s streamlines" % (T_filename, len(T)))
+    return T
 
 
 def clustering(S_dr, k, b=100, t=100):
@@ -167,35 +184,78 @@ def graph_matching_two_clusters(cluster_A, cluster_B, alpha=0.5,
     return corresponding_streamlines
 
 
-if __name__ == '__main__':
-    np.random.seed(0)
+def graph_matching_all_corresponding_pairs(T_A, T_B, k,
+                                           T_A_cluster_labels,
+                                           T_B_cluster_labels,
+                                           corresponding_clusters,
+                                           alpha=0.5, max_iter1=100,
+                                           max_iter2=100):
+    """Loop over all pairs of correponding clusters and perform graph
+    matching between the streamlines of correponding clusters.
 
-    k = 1000  # number of clusters
+    This code executes parallel (multicore) for-loop, if joblib is
+    available. If not, it reverts to a standard for-loop.
+    """
+    print("Compute graph-matching between streamlines of corresponding clusters")
+    correspondence_gm = -np.ones(len(T_A), dtype=np.int)  # container of the results
+    if joblib_available:
+        print("Parallel version")
+        n_jobs = -1
+        clusters_A_idx = [np.where(T_A_cluster_labels == i)[0] for i in range(k)]
+        clusters_A = [T_A[clA_idx] for clA_idx in clusters_A_idx]
+        clusters_B_idx = [np.where(T_B_cluster_labels == corresponding_clusters[i])[0] for i in range(k)]
+        clusters_B = [T_B[clB_idx] for clB_idx in clusters_B_idx]
+        results = Parallel(n_jobs=n_jobs, verbose=True)(delayed(graph_matching_two_clusters)(clusters_A[i], clusters_B[i], alpha=alpha, max_iter1=max_iter1, max_iter2=max_iter2, parallel=False) for i in range(k))
+        # merge results
+        for i in range(k):
+            tmp = results[i] != -1
+            correspondence_gm[clusters_A_idx[i][tmp]] = clusters_B_idx[i][results[i][tmp]]
 
-    # T_A_filename = 'data/HCP_subject124422_100Kseeds/tracks_dti_100K.trk'
-    # T_B_filename = 'data/HCP_subject124422_100Kseeds/tracks_dti_100K.trk'
-    T_A_filename = 'data2/100307/Tractogram/tractogram_b1k_1.25mm_csd_wm_mask_eudx1M.trk'
-    T_B_filename = 'data2/100408/Tractogram/tractogram_b1k_1.25mm_csd_wm_mask_eudx1M.trk'
-    # T_B_filename = T_A_filename
+    else:
+        for i in range(k):
+            print("Graph matching between streamlines of corresponding clusters: cl_A=%s <-> cl_B=%s" % (i, corresponding_clusters[i]))
+            cluster_A_idx = np.where(T_A_cluster_labels == i)[0]
+            cluster_A = T_A[cluster_A_idx]
+            cluster_B_idx = np.where(T_B_cluster_labels == corresponding_clusters[i])[0]
+            cluster_B = T_B[cluster_B_idx]
+            corresponding_streamlines = graph_matching_two_clusters(cluster_A,
+                                                                    cluster_B,
+                                                                    max_iter1=max_iter1,
+                                                                    max_iter2=max_iter2)
 
+            tmp = corresponding_streamlines != -1
+            correspondence_gm[cluster_A_idx[tmp]] = cluster_B_idx[corresponding_streamlines[tmp]]
+
+    return correspondence_gm
+
+
+def fill_missing_correspondences(correspondence_gm, T_A_dr):
+    """After graph matching, in case some correspondeces are missing,
+    i.e. their target index is '-1', fill them following this idea:
+    for a given streamline T_A[i], its correponding one in T_B is the
+    one corresponding to the nearest neighbour of T_A[i] in T_A.
+
+    The (approximate nearest neighbour) is computed with a KDTree on
+    the dissimilarity representation of T_A, i.e. T_A_dr.
+    """
+    print("Filling missing correspondences in T_A with the corresponding to their nearest neighbour in T_A")
+    correspondence = correspondence_gm.copy()
+    T_A_corresponding_idx = np.where(correspondence != -1)[0]
+    T_A_missing_idx = np.where(correspondence == -1)[0]
+    T_A_corresponding_kdt = KDTree(T_A_dr[T_A_corresponding_idx])
+    T_A_missing_NN = T_A_corresponding_kdt.query(T_A_dr[T_A_missing_idx], k=1, return_distance=False).squeeze()
+    correspondence[T_A_missing_idx] = correspondence[T_A_corresponding_idx[T_A_missing_NN]]
+    return correspondence
+    
+
+def alignment_as_graph_matching(T_A_filename, T_B_filename,
+                                k, threshold_short_streamlines=10.0,
+                                b=100, t=100, alpha=0.5, max_iter1=100,
+                                max_iter2=100):
     # 1) load T_A and T_B
-    print("Loading %s" % T_A_filename)
-    T_A, hdr_A = trackvis.read(T_A_filename, as_generator=False)
-    print("Loading %s" % T_B_filename)
-    T_B, hdr_B = trackvis.read(T_B_filename, as_generator=False)
-    T_A = np.array([s[0] for s in T_A], dtype=np.object)
-    T_B = np.array([s[0] for s in T_B], dtype=np.object)
-    print("T_A: %s streamlines" % len(T_A))
-    print("T_B: %s streamlines" % len(T_B))
-
-    # 1.1) Removing short artifactual streamlines
-    threshold = 5.0
-    print("Removing (presumably artifactual) streamlines shorter than %s" % threshold)
-    T_A = np.array([s for s in T_A if length(s) >= threshold], dtype=np.object)
-    T_B = np.array([s for s in T_B if length(s) >= threshold], dtype=np.object)
-    print("T_A: %s streamlines" % len(T_A))
-    print("T_B: %s streamlines" % len(T_B))
-
+    T_A = load_tractogram(T_A_filename, threshold_short_streamlines=threshold_short_streamlines)
+    T_B = load_tractogram(T_B_filename, threshold_short_streamlines=threshold_short_streamlines)
+    
     # 2) Compute the dissimilarity representation of T_A and T_B
     print("Computing the dissimilarity representation of T_A")
     T_A_dr, prototypes_A = compute_dissimilarity(T_A)
@@ -226,51 +286,58 @@ if __name__ == '__main__':
 
     # 5) For each pair corresponding cluster, compute graph matching
     # between their streamlines
-    print("Compute graph-matching between streamlines of corresponding clusters")
-    correspondence_gm = -np.ones(len(T_A), dtype=np.int)  # container of the results
-    if joblib_available:
-        print("Parallel version")
-        n_jobs = -1
-        clusters_A_idx = [np.where(T_A_cluster_labels == i)[0] for i in range(k)]
-        clusters_A = [T_A[clA_idx] for clA_idx in clusters_A_idx]
-        clusters_B_idx = [np.where(T_B_cluster_labels == corresponding_clusters[i])[0] for i in range(k)]
-        clusters_B = [T_B[clB_idx] for clB_idx in clusters_B_idx]
-        results = Parallel(n_jobs=n_jobs, verbose=51)(delayed(graph_matching_two_clusters)(clusters_A[i], clusters_B[i], alpha=alpha, max_iter1=max_iter1, max_iter2=max_iter2, parallel=False) for i in range(k))
-        # merge results
-        for i in range(k):
-            tmp = results[i] != -1
-            correspondence_gm[clusters_A_idx[i][tmp]] = clusters_B_idx[i][results[i][tmp]]
-
-    else:
-        for i in range(k):
-            print("Graph matching between streamlines of corresponding clusters: cl_A=%s <-> cl_B=%s" % (i, corresponding_clusters[i]))
-            cluster_A_idx = np.where(T_A_cluster_labels == i)[0]
-            cluster_A = T_A[cluster_A_idx]
-            cluster_B_idx = np.where(T_B_cluster_labels == corresponding_clusters[i])[0]
-            cluster_B = T_B[cluster_B_idx]
-            corresponding_streamlines = graph_matching_two_clusters(cluster_A,
-                                                                    cluster_B,
-                                                                    max_iter1=max_iter1,
-                                                                    max_iter2=max_iter2)
-
-            tmp = corresponding_streamlines != -1
-            correspondence_gm[cluster_A_idx[tmp]] = cluster_B_idx[corresponding_streamlines[tmp]]
+    correspondence_gm = graph_matching_all_corresponding_pairs(T_A, T_B, k,
+                                                               T_A_cluster_labels,
+                                                               T_B_cluster_labels,
+                                                               corresponding_clusters,
+                                                               alpha=alpha,
+                                                               max_iter1=max_iter1,
+                                                               max_iter2=max_iter2)
 
     # 6) Filling the missing correspondences in T_A with the
-    # corresponding streamline to their nearest neighbour in T_A
-    print("Filling the missing correspondences in T_A with the corresponding to their nearest neighbour in T_A")
-    correspondence = correspondence_gm.copy()
-    T_A_corresponding_idx = np.where(correspondence != -1)[0]
-    T_A_missing_idx = np.where(correspondence == -1)[0]
-    T_A_corresponding_kdt = KDTree(T_A_dr[T_A_corresponding_idx])
-    T_A_missing_NN = T_A_corresponding_kdt.query(T_A_dr[T_A_missing_idx], k=1, return_distance=False).squeeze()
-    correspondence[T_A_missing_idx] = correspondence[T_A_corresponding_idx[T_A_missing_NN]]
+    correspondence = fill_missing_correspondences(correspondence_gm, T_A_dr)
 
     # 7) Compute the mean distance of corresponding streamlines, to
     # check the quality of the result
     distances = distance_corresponding(T_A, T_B, correspondence)
     print("Median distance of corresponding streamlines: %s" % np.median(distances))
 
+    return correspondence, distances
+
+
+if __name__ == '__main__':
+    np.random.seed(0)
+
+    T_A_filename = 'data/HCP_subject124422_100Kseeds/tracks_dti_100K.trk'
+    T_B_filename = 'data/HCP_subject124422_100Kseeds/tracks_dti_100K.trk'
+    # T_A_filename = 'data2/100307/Tractogram/tractogram_b1k_1.25mm_csd_wm_mask_eudx1M.trk'
+    # T_B_filename = 'data2/100408/Tractogram/tractogram_b1k_1.25mm_csd_wm_mask_eudx1M.trk'
+    # T_B_filename = T_A_filename
+
+    # Main parameters:
+    k = 300  # number of clusters
+    threshold_short_streamlines = 15.0
+
+    # Additional internal parameters, no need to change them, usually:
+    b = 100
+    t = 100
+    alpha = 0.5
+    max_iter1 = 100
+    max_iter2 = 100
+
+    correspondence, distances = alignment_as_graph_matching(T_A_filename, T_B_filename,
+                                                            k=k,
+                                                            threshold_short_streamlines=threshold_short_streamlines,
+                                                            b=b,
+                                                            t=t,
+                                                            alpha=alpha,
+                                                            max_iter1=max_iter1,
+                                                            max_iter2=max_iter1)
+
+    print("Saving the result into correspondence.csv")
+    result = np.vstack([range(len(correspondence)), correspondence]).T
+    np.savetxt("correspondence.csv", result, delimiter=",")
+    
     import matplotlib.pyplot as plt
     plt.interactive(True)
     plt.figure()
